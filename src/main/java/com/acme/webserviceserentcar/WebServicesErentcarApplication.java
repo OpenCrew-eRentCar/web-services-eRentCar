d29c609a96d2ded541befb9a1a1639c4b1c9c410
SebastianBran <sebastianbranzapata@gmail.com>
2021-10-30 21:27:29 -0500

    first commit

---
+++ src/main/java/com/acme/webserviceserentcar/WebServicesErentcarApplication.java
@@ -0,0 +1,13 @@
+package com.acme.webserviceserentcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesErentcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesErentcarApplication.class, args);
+    }
+
+}
