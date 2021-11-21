d976612a5cf4e3fb3d3bc5c0623d0a7f729d8554
MiguelTucto <u201521691@upc.edu.pe>
2021-11-20 23:30:01 -0500

    feat: category car test added

--- src/main/java/com/acme/webserviceserentcar/car/resource/CarBrandResource.java
+++ src/main/java/com/acme/webserviceserentcar/car/resource/CarBrandResource.java
@@ -1,10 +1,12 @@
 package com.acme.webserviceserentcar.car.resource;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@With
 public class CarBrandResource {
     private Long id;
     private String name;
