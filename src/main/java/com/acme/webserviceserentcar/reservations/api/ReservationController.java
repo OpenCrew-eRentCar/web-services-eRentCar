365bc1172ec6f962368f5c3c549cf31d2a8c633a
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-09-02 22:50:08 -0500

    feat: refactor reservation endpoints ( #16 )

--- src/main/java/com/acme/webserviceserentcar/reservations/api/ReservationController.java
+++ src/main/java/com/acme/webserviceserentcar/reservations/api/ReservationController.java
@@ -1,63 +1,40 @@
 package com.acme.webserviceserentcar.reservations.api;
 
-import com.acme.webserviceserentcar.reservations.domain.model.entity.Reservation;
+import com.acme.webserviceserentcar.rent.domain.model.entity.Rent;
+import com.acme.webserviceserentcar.rent.mapping.RentMapper;
+import com.acme.webserviceserentcar.rent.resource.RentResource;
 import com.acme.webserviceserentcar.reservations.domain.service.ReservationService;
-import com.acme.webserviceserentcar.reservations.mapping.ReservationMapper;
-import com.acme.webserviceserentcar.reservations.resource.ReservationResource;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/reservations")
 @CrossOrigin
 public class ReservationController {
     private final ReservationService reservationService;
-    private final ReservationMapper reservationMapper;
+    private final RentMapper rentMapper;
 
-    public ReservationController(ReservationService reservationService, ReservationMapper reservationMapper) {
+    public ReservationController(ReservationService reservationService, RentMapper rentMapper) {
         this.reservationService = reservationService;
-        this.reservationMapper = reservationMapper;
+        this.rentMapper = rentMapper;
     }
 
-    @Operation(summary = "Get Reservation by Id", description = "Get Reservation by Id", tags = {"Reservations"})
+    @Operation(summary = "Get Reservation by Car Owner", description = "Get Reservation by Car Owner", tags = {"Reservations"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Reservation returned",
                     content = @Content(
                             mediaType = "application/json",
-                            schema = @Schema(implementation = Reservation.class)
+                            schema = @Schema(implementation = Rent.class)
                     ))
     })
-    @GetMapping("{reservationId}")
-    public ReservationResource getReservationById(@PathVariable Long reservationId) {
-        return reservationMapper.toResource(reservationService.getById(reservationId));
-    }
-
-    @Operation(summary = "Get Reservation by Client Id", description = "Get Reservation by Client Id", tags = {"Reservations"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reservation returned",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = Reservation.class)
-                    ))
-    })
-    @GetMapping("/client/{clientId}")
-    public List<ReservationResource> getReservationByClientId(@PathVariable Long clientId) {
-        return reservationMapper.toResources(reservationService.getAllByClientId(clientId));
-    }
-
-    @Operation(summary = "Delete Reservation", description = "Delete Reservation", tags = {"Reservations"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reservation deleted", content = @Content(mediaType = "application/json"))
-    })
-    @DeleteMapping("{reservationId}")
-    public ResponseEntity<?> deleteReservation(@PathVariable Long reservationId) {
-        return reservationService.delete(reservationId);
+    @GetMapping()
+    public Page<RentResource> getReservationByClientId(Pageable pageable) {
+        return rentMapper.modelListToPage(reservationService.getAll(), pageable);
     }
 }
--- src/main/java/com/acme/webserviceserentcar/reservations/domain/service/ReservationService.java
+++ src/main/java/com/acme/webserviceserentcar/reservations/domain/service/ReservationService.java
@@ -1,13 +1,9 @@
 package com.acme.webserviceserentcar.reservations.domain.service;
 
-import com.acme.webserviceserentcar.reservations.domain.model.entity.Reservation;
-import org.springframework.http.ResponseEntity;
+import com.acme.webserviceserentcar.rent.domain.model.entity.Rent;
 
 import java.util.List;
 
 public interface ReservationService {
-    Reservation getById(Long reservationId);
-    List<Reservation> getAllByClientId(Long clientId);
-    Reservation create(Long clientId, Long rentId);
-    ResponseEntity<?> delete(Long reservationId);
+    List<Rent> getAll();
 }
