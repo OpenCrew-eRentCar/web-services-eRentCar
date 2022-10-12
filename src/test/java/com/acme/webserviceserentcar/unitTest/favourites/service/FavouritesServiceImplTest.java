cf13498091ebc63061b78d9afb98f891b1da827d
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-10-12 18:40:25 -0500

    feat: implemented favourite unit test ( #33 )

--- src/test/java/com/acme/webserviceserentcar/unitTest/favourites/service/FavouritesServiceImplTest.java
+++ src/test/java/com/acme/webserviceserentcar/unitTest/favourites/service/FavouritesServiceImplTest.java
@@ -17,6 +17,10 @@ import org.mockito.MockitoAnnotations;
 
 import javax.validation.Validator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -37,10 +41,10 @@ public class FavouritesServiceImplTest {
     private FavouriteServiceImpl favouriteService;
 
     private CreateFavouriteResource createFavouriteResource;
-    private Favourite favourite;
+    private List<Favourite> favourites;
+    private Favourite favourite1;
     private Client client1;
-    private Client client2;
-    private Car car;
+    private Car car1;
 
     @BeforeEach
     void setUp() {
@@ -52,36 +56,69 @@ public class FavouritesServiceImplTest {
         client1 = new Client();
         client1.setId(1L);
 
-        client2 = new Client();
+        Client client2 = new Client();
         client2.setId(2L);
 
-        car = new Car();
-        car.setId(1L);
-        car.setClient(client2);
+        car1 = new Car();
+        car1.setId(1L);
+        car1.setClient(client2);
 
-        favourite = new Favourite();
-        favourite.setClient(client1);
-        favourite.setCar(car);
+        Car car2 = new Car();
+        car2.setId(2L);
+        car2.setClient(client2);
+
+        favourite1 = new Favourite();
+        favourite1.setId(1L);
+        favourite1.setCar(car1);
+        favourite1.setClient(client1);
+
+        Favourite favourite2 = new Favourite();
+        favourite2.setId(2L);
+        favourite2.setCar(car2);
+        favourite2.setClient(client1);
+
+        favourites = new ArrayList<>();
+        favourites.add(favourite1);
+        favourites.add(favourite2);
     }
 
     @Test
-    void getAllFavourites() {}
+    void getAllFavourites() {
+        when(clientService.getByToken()).thenReturn(client1);
+        when(favouriteRepository.findByClientId(client1.getId())).thenReturn(favourites);
+        List<Favourite> results = favouriteService.getAll();
+        assertEquals(favourites, results);
+    }
 
     @Test
-    void getFavouriteById() {}
+    void getFavouriteById() {
+        when(favouriteRepository.findById(favourite1.getId())).thenReturn(java.util.Optional.ofNullable(favourite1));
+        Favourite result = favouriteService.getById(favourite1.getId());
+        assertEquals(favourite1, result);
+    }
 
     @Test
     void saveFavourite() {
         when(clientService.getByToken()).thenReturn(client1);
         when(clientService.getById(client1.getId())).thenReturn(client1);
-        when(carService.getById(createFavouriteResource.getCarId())).thenReturn(car);
-        when(favouriteMapper.toModel(createFavouriteResource)).thenReturn(favourite);
-        when(favouriteRepository.save(favourite)).thenReturn(favourite);
+        when(carService.getById(createFavouriteResource.getCarId())).thenReturn(car1);
+        when(favouriteMapper.toModel(createFavouriteResource)).thenReturn(favourite1);
+        when(favouriteRepository.save(favourite1)).thenReturn(favourite1);
         Favourite result = favouriteService.create(createFavouriteResource);
-        assertEquals(favourite, result);
-        verify(favouriteRepository).save(favourite);
+        assertEquals(favourite1, result);
+        verify(favouriteRepository).save(favourite1);
     }
 
     @Test
-    void deleteFavourite() {}
+    void deleteFavourite() {
+        when(clientService.getByToken()).thenReturn(client1);
+        when(favouriteRepository.existsByIdAndClientId(favourite1.getId(), client1.getId())).thenReturn(true);
+        when(favouriteRepository.findById(favourite1.getId()))
+                .thenReturn(Optional.of(favourites.get(favourite1.getId().intValue())));
+        favouriteService.delete(client1.getId());
+
+        favourites.remove(favourite1);
+        List<Favourite> result = favourites;
+        assertEquals(favourites, result);
+    }
 }
