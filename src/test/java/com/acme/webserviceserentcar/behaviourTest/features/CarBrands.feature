93f4e0e36b8f318e0cd15b6ef57c6e2cd17d16e6
csgt <u201920785@upc.edu.pe>
2022-10-23 13:58:00 -0500

    Feat(CarBrand):BehaviorTest implemented.

--- src/test/java/com/acme/webserviceserentcar/behaviourTest/features/CarBrands.feature
+++ src/test/java/com/acme/webserviceserentcar/behaviourTest/features/CarBrands.feature
@@ -1,7 +1,48 @@
 Feature: CarBrands Service
-  #TODO: Implement the following Car Brand scenarios
+
   Scenario: Get All Car Brands
+    Given I am Admin Register User
+    And Exist the following Car Brands in the repository
+      | id | name  |
+      | 1  | Onda  |
+      | 2  | Kia   |
+    When I get all Car Brands
+    Then I should get a list of Car Brands with length 2
+
   Scenario: Get Car Brand by Id
+    Given I am Admin Register User
+    And Exist the following Car Brands in the repository
+      | id | name |
+      | 1  | Onda |
+      | 2  | Kia  |
+    When I get Car Brand with id 1
+    Then I should get a Car Brand with id 1
+
   Scenario: Create Car Brand
+    Given I am a User
+    And I want to register a brand
+    When I complete the register in the system with data
+      | id | name |
+      | 1  | Onda |
+    Then I should get a Car Brand with id 1
+
   Scenario: Update Car Brand
+    Given I am a User
+    And I have a Car Brand
+      | id | name |
+      | 1  | Onda |
+    When I update the data with
+      | id | name |
+      | 1  | Kia  |
+    Then I should get a Car Brand with id 1 and names "Kia"
+
   Scenario: Delete Car Brand
+    Given I am a User
+    And I have these Car Brand
+      | id | name |
+      | 1  | Onda  |
+      | 2  | Kia  |
+    When I delete a Car Brand
+      | id | name |
+      | 1  | Onda  |
+    Then I should not get a Car Brand with id 1
--- src/test/java/com/acme/webserviceserentcar/behaviourTest/steps/CarBrandsStepDefinition.java
+++ src/test/java/com/acme/webserviceserentcar/behaviourTest/steps/CarBrandsStepDefinition.java
@@ -1,4 +1,87 @@
 package com.acme.webserviceserentcar.behaviourTest.steps;
 
+import com.acme.webserviceserentcar.car.domain.model.entity.CarBrand;
+import com.acme.webserviceserentcar.car.domain.persistence.CarBrandRepository;
+import com.acme.webserviceserentcar.car.service.CarBrandServiceImpl;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.mockito.*;
+
+import javax.validation.Validator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
 public class CarBrandsStepDefinition {
+    @Mock
+    private CarBrandRepository carBrandRepository;
+
+    @Mock
+    private Validator validator;
+
+    @InjectMocks
+    @Spy
+    private CarBrandServiceImpl carBrandService;
+
+    private List<CarBrand> carBrands;
+
+    private CarBrand carBrand;
+
+    private List<CarBrand> results;
+
+    private CarBrand result;
+
+    @Before
+    public void setUp(){MockitoAnnotations.openMocks(this);}
+
+    @Given("I am Admin Register User")
+    public void iAmAClient() {}
+
+    @And("Exist the following Car Brands in the repository")
+    public void existTheFollowingCarBrandInTheRepository(DataTable table){
+        List<List<String>> rows = table.cells().stream().skip(1).toList();
+
+        carBrands = new ArrayList<>();
+        for (List<String> row : rows){
+            CarBrand carBrand = new CarBrand();
+            carBrand.setId(Long.parseLong(row.get(0)));
+            carBrand.setName(row.get(1));
+            carBrands.add(carBrand);
+        }
+    }
+
+    @When("I get all Car Brands")
+    public void iGetAllCarBrands(){
+        when(carBrandRepository.findAll()).thenReturn(carBrands);
+        results = carBrandService.getAll();
+    }
+
+    @Then("I should get a list of Car Brands with length {int}")
+    public void iShouldGetAListOfCarBrandWithLength(int length){
+        assertEquals(length,results.size());
+        assertEquals(carBrands, results);
+    }
+
+    @When("I get Car Brand with id {long}")
+    public void iGetCarBrandWithId(Long carBrandId){
+        when(carBrandRepository.findById(carBrandId))
+                .thenReturn(Optional.of(carBrands.get(carBrandId.intValue() - 1)));
+        result = carBrandService.getById(carBrandId);
+    }
+
+    @Then("I should get a Car Brand with id {int}")
+    public void iShouldGetACarBrandWithID(int carBrandId){
+        assertEquals(carBrandId, result.getId());
+    }
+
+
+
 }
