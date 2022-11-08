76286113ca56635f9303dcc111429fda7e085424
ClinderSGT <93946818+ClinderSGT@users.noreply.github.com>
2022-11-08 11:37:37 -0500

    Feat: Complete carModel unit test

--- src/test/java/com/acme/webserviceserentcar/unitTest/carModels/service/CarModelsServiceImplTest.java
+++ src/test/java/com/acme/webserviceserentcar/unitTest/carModels/service/CarModelsServiceImplTest.java
@@ -1,23 +1,18 @@
 package com.acme.webserviceserentcar.unitTest.carModels.service;
 
-
 import com.acme.webserviceserentcar.car.domain.model.entity.CarBrand;
+import com.acme.webserviceserentcar.car.domain.persistence.CarBrandRepository;
 import com.acme.webserviceserentcar.car.domain.model.entity.CarModel;
 import com.acme.webserviceserentcar.car.domain.persistence.CarModelRepository;
-
 import com.acme.webserviceserentcar.car.domain.service.CarBrandService;
-
 import com.acme.webserviceserentcar.car.mapping.CarModelMapper;
 import com.acme.webserviceserentcar.car.resource.create.CreateCarModelResource;
 import com.acme.webserviceserentcar.car.resource.update.UpdateCarModelResource;
 import com.acme.webserviceserentcar.car.service.CarModelServiceImpl;
 import com.acme.webserviceserentcar.client.domain.model.entity.Client;
-
 import com.acme.webserviceserentcar.client.service.ClientServiceImpl;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import org.mockito.*;
 import javax.validation.Validator;
 import java.util.ArrayList;
@@ -31,7 +26,8 @@ public class CarModelsServiceImplTest {
 
     @Mock
     private CarModelRepository carModelRepository;
-
+    @Mock
+    private CarBrandRepository carBrandRepository;
     @Mock
     private CarModelMapper carModelMapper;
     @Mock
@@ -45,14 +41,10 @@ public class CarModelsServiceImplTest {
     @InjectMocks
     @Spy
     private CarModelServiceImpl carModelService;
-
-    private  UpdateCarModelResource updateCarModelResource;
-
     private CreateCarModelResource createCarModelResource;
     private List<CarModel> carModels;
     private Client client1;
     private CarModel carModel;
-
     private CarBrand carBrand;
 
     @BeforeEach
@@ -65,34 +57,25 @@ public class CarModelsServiceImplTest {
         client1 = new Client();
         client1.setId(1L);
 
-        Client client2 = new Client();
-        client2.setId(2L);
-
         carBrand = new CarBrand();
         carBrand.setId(1L);
 
-
-        CarBrand car2 = new CarBrand();
-        car2.setId(2L);
-
-
+        carModel = new CarModel();
+        carModel.setCarBrand(carBrand);
+        carModel.setName("Audi");
+        carModel.setImagePath("www.image.com");
 
         CarModel carModel1 = new CarModel();
         carModel1.setId(2L);
         carModel1.setName("Onda");
-
         CarModel carModel2 = new CarModel();
         carModel2.setId(3L);
         carModel2.setName("kia");
-
-        carModel = new CarModel();
-        carModel.setId(1L);
-        carModel.setName("Audi");
-        carModel.setImagePath("www.image.com");
-
+        
         carModels = new ArrayList<>();
         carModels.add(carModel1);
         carModels.add(carModel2);
+
     }
 
     @Test
@@ -111,30 +94,43 @@ public class CarModelsServiceImplTest {
 
     @Test
     void createCarModel(){
-        /*when(clientService.getByToken()).thenReturn(client1);
-        when(clientService.getById(client1.getId())).thenReturn(client1);
-        when(carBrandService.getById(createCarModelResource.getCardBrandId())).thenReturn(carBrand);
+        CreateCarModelResource createCarModelResource = new CreateCarModelResource();
+        createCarModelResource.setCardBrandId(1L);
+
+        carModel = new CarModel();
+        carModel.setId(carModel.getId());
+        carModel.setName(carModel.getName());
+        carModel.setCarBrand(carBrand);
+
+        when(carBrandRepository.findById(createCarModelResource.getCardBrandId())).thenReturn(Optional.of(carBrand));
         when(carModelMapper.toModel(createCarModelResource)).thenReturn(carModel);
         when(carModelRepository.save(carModel)).thenReturn(carModel);
+
         CarModel result = carModelService.create(createCarModelResource);
         assertEquals(carModel, result);
-        verify(carModelRepository).save(carModel);*/
+        verify(carModelRepository).save(carModel);
     }
 
     @Test
     void updateCarModel() {
-        /*CarModel request = new CarModel();
-        UpdateCarModelResource request1 = new UpdateCarModelResource();
-        request.setId(1L);
-        request1.setName("Audi");
-        request1.setImagePath("www.image-update.com");
+        UpdateCarModelResource request = new UpdateCarModelResource();
+        request.setName(carModel.getName());
+        request.setCardBrandId(1L);
+        request.setImagePath("www.image-update.com");
 
-        when(carModelRepository.findById(carModel.getId())).thenReturn(Optional.of(request));
-        when(carModelRepository.save(request)).thenReturn(request);
-        CarModel result = carModelService.update(carModel.getId(), request1);
-        assertEquals(request1, result);
-        verify(carModelRepository).save(request);*/
+        CarModel carModelUpdate = new CarModel();
+        carModelUpdate.setId(carModel.getId());
+        carModelUpdate.setName(carModel.getName());
+        carModelUpdate.setCarBrand(carBrand);
+        carModelUpdate.setImagePath(request.getImagePath());
 
+        when(carBrandRepository.findById(request.getCardBrandId())).thenReturn(Optional.of(carBrand));
+        when(carModelRepository.save(carModelUpdate)).thenReturn(carModelUpdate);
+        when(carModelRepository.findById(carModelUpdate.getId())).thenReturn(Optional.of(carModelUpdate));
+
+        CarModel result = carModelService.update(carModelUpdate.getId(), request);
+        assertEquals(carModelUpdate, result);
+        verify(carModelRepository).save(carModelUpdate);
     }
 
     @Test
